0d7da1aeff0bbbc7ab0a1bbbed08fe561f5b5358
Jamil van de Ree <jgjvanderee@gmail.com>
2024-09-23 16:39:31 +0200

    Regex

---
+++ week03/quiz.py
@@ -0,0 +1,24 @@
+# score = 0
+
+# if input("Capital of France? ").lower() == "paris":
+#     score += 1
+# if input("5 + 7 = ? ") == "12":
+#     score += 1
+# if input("Who wrote 'Hamlet'? ").lower() == "shakespeare":
+#     score += 1
+
+# print(f"Your score: {score}/3")
+
+a = 10
+b = 5
+c = 2
+
+result = a * b + c - (b // c)
+
+# What is the value of 'result'?
+1 x 2 = 2
+1 x 4 = 4
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
