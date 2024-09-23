465de7c4bddeb90ecae57c3e3e5c80bf46780d6a
Jamil van de Ree <jgjvanderee@gmail.com>
2024-09-23 15:53:23 +0200

    Update code5.py

--- week03/Analysis/CA5/code5.py
+++ week03/Analysis/CA5/code5.py
@@ -2,7 +2,7 @@
 sentence = "I just came to say hello!"
 for c in sentence:
 	print(c)
- 
+
 #  •	Expected Output: This code will produce the same output as Code 4,
 #  printing each character of the sentence on a new line. The loop iterates
 #  directly over the string, which is equivalent to using range(len(sentence)).
