f51218d63c5dd4df7031abe9db1271be2b7d9a8f
Jamil van de Ree <jgjvanderee@gmail.com>
2024-10-18 22:17:56 +0200

    Update morsecodetranslator.py

--- week06/Problems/A2W6P5/morsecodetranslator.py
+++ week06/Problems/A2W6P5/morsecodetranslator.py
@@ -1,4 +1,5 @@
 def message_to_morse(message):
+    """Converts a text message to Morse code."""
     morse_dict = {
         "A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".",
         "F": "..-.", "G": "--.", "H": "....", "I": "..", "J": ".---",
@@ -7,24 +8,48 @@ def message_to_morse(message):
         "U": "..-", "V": "...-", "W": ".--", "X": "-..-", "Y": "-.--",
         "Z": "--..", "1": ".----", "2": "..---", "3": "...--", "4": "....-",
         "5": ".....", "6": "-....", "7": "--...", "8": "---..", "9": "----.",
-        "0": "-----", " ": "   "  # 3 spaces for separating words
+        "0": "-----", " ": "   "  # 3 spaces for word separation
     }
 
-    morse_code = ""
-    unsupported_chars = []
+    morse_code = []
 
     for char in message.upper():
         if char in morse_dict:
-            morse_code += morse_dict[char] + " "
+            morse_code.append(morse_dict[char])
         else:
-            unsupported_chars.append(char)
+            return f"Can't convert char [{char}] if there is no mapping for specific characters."
 
-    if unsupported_chars:
-        return f"Can't convert char(s) {', '.join(unsupported_chars)} if there is no mapping for specific characters."
+    return ' '.join(morse_code)
 
-    return morse_code.strip()
+
+def morse_to_message(morse_code):
+    """Converts Morse code to a text message."""
+    morse_dict = {value: key for key, value in message_to_morse().items()}
+
+    message = []
+    words = morse_code.split('   ')
+
+    for word in words:
+        chars = word.split(' ')
+        message.append(''.join(morse_dict.get(char, '') for char in chars))
+
+    return ' '.join(message)
+
+
+def translate_text(text, direction="to_morse"):
+    """Translates text to or from Morse code."""
+    if direction == "to_morse":
+        return message_to_morse(text)
+    elif direction == "from_morse":
+        return morse_to_message(text)
+    else:
+        raise ValueError(
+            "Invalid direction. Choose 'to_morse' or 'from_morse'.")
 
 
 if __name__ == "__main__":
-    message = input("Enter a message: ")
-    print(message_to_morse(message))
+    text = input()
+    direction = input("Enter 'to_morse' or 'from_morse': ")
+
+    result = translate_text(text, direction)
+    print(result)
