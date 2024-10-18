a75666b7d791684824fa714d68bd144a54436928
Jamil van de Ree <jgjvanderee@gmail.com>
2024-10-18 21:53:19 +0200

    Update morsecodetranslator.py

--- week06/Problems/A2W6P5/morsecodetranslator.py
+++ week06/Problems/A2W6P5/morsecodetranslator.py
@@ -1,52 +1,28 @@
 def message_to_morse(message):
     morse_dict = {
-        "A": ".-",
-        "B": "-...",
-        "C": "-.-.",
-        "D": "-..",
-        "E": ".",
-        "F": "..-.",
-        "G": "--.",
-        "H": "....",
-        "I": "..",
-        "J": ".---",
-        "K": "-.-",
-        "L": ".-..",
-        "M": "--",
-        "N": "-.",
-        "O": "---",
-        "P": ".--.",
-        "Q": "--.-",
-        "R": ".-.",
-        "S": "...",
-        "T": "-",
-        "U": "..-",
-        "V": "...-",
-        "W": ".--",
-        "X": "-..-",
-        "Y": "-.--",
-        "Z": "--..",
-        "1": ".----",
-        "2": "..---",
-        "3": "...--",
-        "4": "....-",
-        "5": ".....",
-        "6": "-....",
-        "7": "--...",
-        "8": "---..",
-        "9": "----.",
-        "0": "-----",
+        "A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".",
+        "F": "..-.", "G": "--.", "H": "....", "I": "..", "J": ".---",
+        "K": "-.-", "L": ".-..", "M": "--", "N": "-.", "O": "---",
+        "P": ".--.", "Q": "--.-", "R": ".-.", "S": "...", "T": "-",
+        "U": "..-", "V": "...-", "W": ".--", "X": "-..-", "Y": "-.--",
+        "Z": "--..", "1": ".----", "2": "..---", "3": "...--", "4": "....-",
+        "5": ".....", "6": "-....", "7": "--...", "8": "---..", "9": "----.",
+        "0": "-----", " ": "   "  # 3 spaces for separating words
     }
 
     morse_code = ""
+    unsupported_chars = []
+
     for char in message.upper():
         if char in morse_dict:
             morse_code += morse_dict[char] + " "
         else:
-            print(
-                f"Can't convert char {char} if there is no mapping for specific characters.")
-            return ""
-    return morse_code
+            unsupported_chars.append(char)
+
+    if unsupported_chars:
+        return f"Can't convert char(s) {', '.join(unsupported_chars)} if there is no mapping for specific characters."
+
+    return morse_code.strip()
 
 
 if __name__ == "__main__":
